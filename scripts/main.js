43bb4da0e9092169a1a1eaff28325049bfac5af7
shibuya <65008051+Haruo-es@users.noreply.github.com>
2021-12-09 09:09:44 +0900

    サンプルページの動作確認変更

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,7 @@ myImage.onclick = function() {
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
+console.log(myheading)
 
 function setUserName() {
   let myName = prompt('Please enter your name.');
@@ -22,7 +23,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    myHeading.innerHTML = 'こんにちは, ' + myName;
   }
 }
 
@@ -30,7 +31,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'さようなら, ' + storedName;
 }
 
 myButton.onclick = function() {
